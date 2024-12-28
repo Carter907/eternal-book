55cc396db718034337a092ca3c4196b9ce41ec99
Carter <speers.carter@gmail.com>
2024-12-28 15:33:55 -0500

    fixed vars

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwsdsaunavhsxynnlkdx.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY!!
+const supabaseKey = process.env.SUPABASE_ANON_KEY!!
 export const supabase = createClient(supabaseUrl, supabaseKey)
