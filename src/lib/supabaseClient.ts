a4967ad212d3ee244ed05b6429b730176ea9043c
Carter <speers.carter@gmail.com>
2024-12-28 15:39:35 -0500

    key stuff

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwsdsaunavhsxynnlkdx.supabase.co'
-const supabaseKey = process.env.SUPABASE_ANON_KEY!!
-export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const supabase = createClient(supabaseUrl,
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53c2RzYXVuYXZoc3h5bm5sa2R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzNjA2OTEsImV4cCI6MjA1MDkzNjY5MX0.X2SspuDIZTQEsC1Q08kH9ygf74qjZaw_PnS77aev2AY')
