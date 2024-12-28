4746cd1a9fca80127e142da2de4b7bdd5d8de355
Carter <speers.carter@gmail.com>
2024-12-28 16:14:37 -0500

    fixing stuff in production

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwsdsaunavhsxynnlkdx.supabase.co'
-console.log(process.env)
+console.log(JSON.stringify(process.env, null, 2));
 const publicKey = process.env.SUPABASE_KEY!!
 export const supabase = createClient(supabaseUrl, publicKey);
