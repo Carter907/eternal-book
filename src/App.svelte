14dceb3eedcd8d338e357cd6dcae3dd418e31a78
Carter <speers.carter@gmail.com>
2024-12-28 15:29:31 -0500

    added oauth

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-    import svelteLogo from './assets/svelte.svg'
-    import viteLogo from '/vite.svg'
-    import Counter from './lib/Entry.svelte'
+    import { supabase } from './lib/supabaseClient'
 
+
+    supabase.auth.signInWithOAuth({
+        provider: 'google'
+    })
 </script>
 
 <main>
