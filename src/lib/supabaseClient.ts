eda9903cc947304d5bcba4a18fed2329c5304851
Carter <speers.carter@gmail.com>
2024-12-28 16:23:49 -0500

    more tests

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwsdsaunavhsxynnlkdx.supabase.co'
 console.log(JSON.stringify(process.env, null, 2));
-const publicKey = import.meta.env.SUPA_KEY;
-console.log(publicKey);
+const publicKey = import.meta.env.VITE_SUPA_KEY;
 console.log(import.meta.env)
 export const supabase = createClient(supabaseUrl, publicKey);
