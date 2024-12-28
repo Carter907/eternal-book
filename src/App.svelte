0fe70aef28fbe0ac44652eab3353dd3a28d2d0fd
Carter <speers.carter@gmail.com>
2024-12-28 00:10:00 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import svelteLogo from './assets/svelte.svg'
+    import viteLogo from '/vite.svg'
+    import Counter from './lib/Entry.svelte'
+
+</script>
+
+<main>
+    <form class="border p-4 w-[400px] bg-blue-100 flex flex-col space-y-2">
+        <input type="text" name="title">
+        <input type="date">
+        <textarea id="markdown-editor" name="description" rows="15"></textarea>
+    </form>
+    <textarea id="markdown-editor"></textarea>
+
+</main>
+
+<style>
+
+</style>
