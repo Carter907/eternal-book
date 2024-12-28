c14f0ecb6dd2706b742f296b04d9ba5b1ff854d1
Carter <speers.carter@gmail.com>
2024-12-28 16:17:14 -0500

    more testing stuff

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwsdsaunavhsxynnlkdx.supabase.co'
 console.log(JSON.stringify(process.env, null, 2));
-const publicKey = process.env.SUPABASE_KEY!!
+const publicKey = process.env.SUPA_KEY!!;
+console.log(publicKey);
 export const supabase = createClient(supabaseUrl, publicKey);
