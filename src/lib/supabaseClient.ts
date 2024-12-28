6966796455b2c257a0770ab9437296c65229f689
Carter <speers.carter@gmail.com>
2024-12-28 16:20:19 -0500

    fixes

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nwsdsaunavhsxynnlkdx.supabase.co'
 console.log(JSON.stringify(process.env, null, 2));
-const publicKey = process.env.SUPA_KEY!!;
+const publicKey = import.meta.env.SUPA_KEY;
 console.log(publicKey);
+console.log(import.meta.env)
 export const supabase = createClient(supabaseUrl, publicKey);
